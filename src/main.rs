5664d15db036b03a7cf55ec9d769069f3aa8b3f7
david <pc_freak@att.net>
2022-08-10 21:02:20 -0700

    Added world to game state, Position and Renderable components, and registered them with the ecs in the game state

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
-use rltk::{Rltk, GameState};
+use rltk::{GameState, Rltk, VirtualKeyCode, RGB};
+use specs::prelude::*;
+use specs_derive::Component;
+use std::cmp::{max, min};
 
-struct State {}
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Renderable {
+    glyph: rltk::FontCharType,
+    fg: RGB,
+    bg: RGB,
+}
+
+struct State {
+    ecs: World
+}
+
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
@@ -13,6 +32,11 @@ fn main() -> rltk::BError {
     let context = RltkBuilder::simple80x50()
         .with_title("Roguelike Tutorial")
         .build()?;
-    let gs = State{ };
+    let mut gs = State {
+        ecs: World::new()
+    };
+
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
     rltk::main_loop(context, gs)
 }
